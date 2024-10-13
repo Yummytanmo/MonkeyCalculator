0ee6435582dd10d0e6209076e80614f87c770f7c
beidaobeidao <3219558101@qq.com>
2024-10-14 01:04:57 +0800

    比较大小程序，有计时功能

--- BigORSmall.py
+++ BigORSmall.py
@@ -1,29 +1,64 @@
 import pyautogui
 import time
+import cv2
+import pytesseract
+from PIL import ImageGrab
+import numpy as np
+import pytesseract
+import re
+pytesseract.pytesseract.tesseract_cmd = r'F:\Software\Tesseract\tesseract.exe'
+def compare_numbers_in_bbox(bbox):
+    # 截取屏幕
+    img = ImageGrab.grab(bbox)
+
+    # 转换为 OpenCV 格式
+    img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+
+    # 使用 Tesseract OCR 识别文本
+    text = pytesseract.image_to_string(img_cv)
+    print(text)
+    # 使用正则表达式提取数字
+    numbers = re.findall(r'\d+', text)
+
+    # 如果找到了两个数字，则比较大小
+    if len(numbers) >= 2:
+        num1 = int(numbers[0])
+        num2 = int(numbers[1])
+        
+        if num1 > num2:
+            return '>'
+        else:
+            return '<'
+    else:
+        print("未找到足够的数字进行比较")
 
 # 定义数字的绘制函数
 def draw_number(char, start_x, start_y):
     # 基本长度
-    length = 100
+    length = 50
 
     # 设置起始位置
     pyautogui.moveTo(start_x, start_y)
 
     if char == '>':
-        pyautogui.dragRel(length, length, duration=0.2)  # 斜线向下
-        pyautogui.moveRel(-length, 0, duration=0.1)      # 移动到中间
-        pyautogui.dragRel(length, -length, duration=0.2) # 斜线向上
+        pyautogui.mouseDown()
+        pyautogui.dragRel(length, -length)  # 画上斜线
+        pyautogui.dragRel(-length, -length) # 画下斜线
+        pyautogui.mouseUp()
     elif char == '<':
-        pyautogui.dragRel(-length, length, duration=0.2) # 斜线向下
-        pyautogui.moveRel(length, 0, duration=0.1)       # 移动到中间
-        pyautogui.dragRel(-length, -length, duration=0.2)# 斜线向上
+        pyautogui.mouseDown()
+        pyautogui.dragRel(-length, -length) # 斜线向下
+        pyautogui.dragRel(length, -length)# 斜线向上
+        pyautogui.mouseUp()
         
 
 def main():
-    input_char = input("请输入符号 ")
-    start_x = 300 #int(input("请输入起始X坐标: "))
-    start_y = 300 #int(input("请输入起始Y坐标: "))
+
     time.sleep(2)  # 给用户2秒时间调整鼠标到屏幕上
+    start_x = 700 #int(input("请输入起始X坐标: "))
+    start_y = 1000 #int(input("请输入起始Y坐标: "))
+    bbox = (200, 200, 1000, 500)
+    input_char = compare_numbers_in_bbox(bbox)
     draw_number(input_char, start_x, start_y)
 
 if __name__ == "__main__":
